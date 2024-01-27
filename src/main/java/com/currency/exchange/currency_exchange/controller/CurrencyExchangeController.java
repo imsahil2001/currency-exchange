09a64abca598ab17df8401d208ae780a203c13af
imsahil2001 <sahilkkr56@gmail.com>
2024-01-27 16:27:07 +0530

    Added UI calls cors origin fix

--- src/main/java/com/currency/exchange/currency_exchange/controller/CurrencyExchangeController.java
+++ src/main/java/com/currency/exchange/currency_exchange/controller/CurrencyExchangeController.java
@@ -5,10 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.lang.NonNull;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
 @RestController
@@ -18,6 +15,7 @@ public class CurrencyExchangeController {
     @Autowired
     ExchangeService exchangeService;
 
+    @CrossOrigin(origins = "https://currency-calculator-one.vercel.app/")
     @PostMapping("/inr")
     public ResponseEntity<?> exchangeToInr(@NonNull @RequestParam(name = "rate") Double rate){
         return ResponseEntity.status(HttpStatus.OK).body(exchangeService.manipulateRate(rate));
